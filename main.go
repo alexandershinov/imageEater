94e3bddb443d0bcebdaf72fc83e92378101e71c8
Alexander A Shinov <alexandershinov@gmail.com>
2019-02-25 16:16:37 +0300

    main file: http server, empty mux router, toml config, graceful shutdown

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type Config struct {
+	Port       int
+	FilesField string
+	FilesDir   string
+}
+
+var config Config
+
+// read config from file (toml format)
+func ReadConfig(configFile string) Config {
+	defaultConfig := Config{
+		Port:       4000,
+		FilesField: "files",
+		FilesDir:   "files/",
+	}
+
+	_, err := os.Stat(configFile)
+	if err != nil {
+		log.Printf("config file '%s' is missing\n", configFile)
+		return defaultConfig
+	}
+
+	var config Config
+	if _, err := toml.DecodeFile(configFile, &config); err != nil {
+		log.Printf("config file '%s' parsing error, default configuration uses\n", configFile)
+		return defaultConfig
+	}
+
+	return config
+}
+
+func listenAndServe(port string, router *mux.Router) *http.Server {
+	srv := &http.Server{Addr: port, Handler: router}
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %s", err)
+		}
+	}()
+	return srv
+}
+
+func main() {
+	// try to read config
+	configFile := flag.String("config", "config.toml", "config file path")
+	flag.Parse()
+	config = ReadConfig(*configFile)
+
+	// add router
+	router := mux.NewRouter()
+
+	// start http server
+	log.Printf("starting HTTP server")
+	srv := listenAndServe(fmt.Sprintf(":%d", config.Port), router)
+
+	// Setting up signal capturing
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	// Waiting for SIGINT (pkill -2)
+	<-stop
+
+	//log.Printf("serving for 5 seconds")
+	//time.Sleep(5 * time.Second)
+	log.Printf("stopping server")
+	// graceful shutdown
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	if err := srv.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+	log.Printf("exit")
+}
